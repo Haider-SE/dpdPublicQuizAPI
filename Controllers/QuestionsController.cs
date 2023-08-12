6b5415af79bfc79970712d919446f75a873fb085
Haider Ali <haiderali98.ha61@gmail.com>
2023-08-12 17:49:09 +0500

    #13, #14, #15, #16, #18, #19, #20

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -6,19 +6,17 @@ using Microsoft.Extensions.Configuration;
 namespace dpdPublicQuizAPI.Controllers
 {
     [ApiController]
-    [Route("add/question")]
-
     public class QuestionsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
         private readonly IConfiguration _configuration;
-
         public QuestionsController(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context; 
             _configuration = configuration;
         }
         [HttpPost]
+        [Route("add/question")]
         public async Task<IActionResult> AddQuestions(Questions request)
         {
             if(request.QuestionText == null || request.QuestionText == "")
@@ -29,5 +27,59 @@ namespace dpdPublicQuizAPI.Controllers
             await _context.SaveChangesAsync();
             return Ok("Question Added Succesfully");
         }
+        [HttpGet]
+        [Route("getAllQuestions")]
+        public async Task<IActionResult> GetAllQuestions()
+        {
+            var allQuestions = await _context.Questions.ToListAsync();
+            return Ok(allQuestions);
+        }
+        [HttpGet]
+        [Route("getQuestion/{questionId}")]
+        public async Task<IActionResult> GetQuestion(Guid questionId)
+        {
+            var question = await _context.Questions.FindAsync(questionId);
+            if(question == null)
+            {
+                return NotFound("Question Does not Exist");
+            }
+            return Ok(question);
+        }
+
+        [HttpPut]
+        [Route("upadteQuestion/{questionId}")]
+        public async Task<IActionResult> UpdateQuestion(Questions question)
+        {
+            var existingQuestion = await _context.Questions.FindAsync(question);
+            if(existingQuestion == null)
+            {
+                return NotFound("Question Does not Exist");
+            }
+            existingQuestion.QuestionText = question.QuestionText;
+            _context.Entry(existingQuestion).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(existingQuestion);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpDelete]
+        [Route("deleteQuestion/{questionId}")]
+        public async Task<IActionResult> DeleteQuestion(Guid questionId)
+        {
+            var existingQuestion = await _context.Questions.FindAsync(questionId);
+            if(existingQuestion == null)
+            {
+                return NotFound("No such Question Exists");
+            }
+            _context.Questions.Remove(existingQuestion);
+            await _context.SaveChangesAsync();
+            return Ok("Question Deleted Successfully");
+        }
+
     }
 }
