9ec208ea8420a527348870ba045947657c456858
Haider Ali <haiderali98.ha61@gmail.com>
2023-08-13 20:20:56 +0500

    add Teacher

---
+++ Controllers/TeachersController.cs
@@ -0,0 +1,33 @@
+﻿using dpdPublicQuizAPI.Data.ContextConfiguration;
+using dpdPublicQuizAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace dpdPublicQuizAPI.Controllers
+{
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext _context;
+        public TeachersController(IConfiguration configuration, ApplicationDbContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+        [HttpPost]
+        [Route("addTeacher")]
+        public async Task<IActionResult> AddTeacher (Teachers request)
+        {
+            if(request.FirstName == null || request.LastName == null)
+            {
+                return BadRequest("First Name or Last Name cannot be empty");
+            }
+            var addTeacher = await _context.Teachers.AddAsync(request);
+            return Ok(addTeacher);
+        }
+
+    }
+}
