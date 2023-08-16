7efbdb2ea896adda0a8976b50465eb74d5eedb6e
Haider Ali <haiderali98.ha61@gmail.com>
2023-08-16 18:03:45 +0500

    Update Role

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -34,5 +34,25 @@ namespace dpdPublicQuizAPI.Controllers
             var allRoles = await _context.Roles.ToListAsync();
             return Ok(allRoles);
         }
+        [HttpPut]
+        [Route("updateRole/{roleId}")]
+        public async Task<IActionResult> UpdateRole(Roles request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Data Cannot be null");
+            }
+            var existingRole = await _context.Roles.FindAsync(request.RoleID);
+            if(existingRole == null)
+            {
+                return NotFound("The Role was not found");
+
+            }
+            existingRole.RoleName = request.RoleName;
+            _context.Entry(existingRole).State = EntityState.Modified;
+            _context.SaveChangesAsync();
+            return Ok("Roles Detail Updated Successfully");
+
+        }
     }
 }
