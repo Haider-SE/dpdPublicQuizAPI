f53dfff37f09a860a65d40962f94d100a6261c21
Haider Ali <haiderali98.ha61@gmail.com>
2023-08-21 23:09:47 +0500

    #84, #83, #86

--- Models/Users.cs
+++ Models/Users.cs
@@ -7,6 +7,7 @@ namespace dpdPublicQuizAPI.Models
         public Guid Id { get; set; }
         [ForeignKey("Roles")]
         public Guid RoleId{ get; set; }
+        public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
