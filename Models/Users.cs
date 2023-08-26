b6753a3ac813873268687f158da6f10b2ea246fb
Haider Ali <haiderali98.ha61@gmail.com>
2023-08-26 22:49:36 +0500

    few fixes

--- Models/Users.cs
+++ Models/Users.cs
@@ -7,10 +7,10 @@ namespace dpdPublicQuizAPI.Models
         public Guid Id { get; set; }
         [ForeignKey("Roles")]
         public Guid RoleId{ get; set; }
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
